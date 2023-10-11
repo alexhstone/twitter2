990e595c1fe674b2952dd9b06654f4a0686e2987
= <=>
2023-10-11 13:26:05 -0400

    added routes

--- server/server.js
+++ server/server.js
@@ -25,6 +25,33 @@ app.get('/api/users/', async (req, res) => {
    res.send(users);
 })
 
+//get user by id
+app.get('/api/users/:id', async (req, res) => {
+    const user = await dao.getUser(req.params.id)
+    
+    if (!user){
+        res.statusCode(404).send('not found')
+    }
+
+    res.send(user)
+})
+
+//get comments by employee id (comments left BY users)
+app.get('/api/comments/employee/:id', async (req, res) =>  {
+    
+    const comments = await dao.getCommentsByEmployeeId(req.params.id)
+
+    res.send(comments)
+})
+
+
+//get comments by manager id (comments left FOR user)
+app.get('/api/comments/manager/:id', async (req, res) =>  {
+    
+    const comments = await dao.getCommentsByManagerId(req.params.id)
+
+    res.send(comments)
+})
 
 
 app.listen(port);
