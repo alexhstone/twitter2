e81b82875cf5caac87f419f7de0036608520d7f8
= <=>
2023-10-11 14:37:06 -0400

    some react component hook up

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,12 +1,16 @@
 import {React, useState, useEffect} from 'react';
 
 
-const Comment = (comment) => {
+const Comment = ({comment}) => {
 
     return (
         <div className="comment">
-            <p>{comment.comment}</p>
+            <p>{comment?.comment}</p>
+            {//comment?.followups.map(p => <p>{p}</p>)
+            }
             <button>respond</button>
         </div>
     )
 }
+
+export default Comment;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,36 @@
 import {React, useState, useEffect} from 'react';
+import Comment from './Comment';
 
+
+const fetchComments = async (id) => {
+    await fetch(`localhost:3030/api/comments/employee/${id}`)
+    .then(res => res.json())
+
+}
 
 const Home = () => {
+    
+    const [comments, setComments] = useState([]);
+    
+    const fetchComments = async (id) => {
+        await fetch(`http://localhost:3030/api/comments/employee/${id}`)
+        .then(res => res.json())
+        .then(comments => setComments(comments))
+    }
+
+    useEffect(() => { 
+        fetchComments("zzz9969")
+    },[])
+
     return (
         <div>
-            <p>Hello World!</p>
+           <div>
+            <h2>Comments FROM me</h2>
+            {comments.length && comments.map(comment => <Comment comment={comment} key={comment?._id} />)}
+           </div>
         </div>
     )
 }
 
 
-export default Home
+export default Home;
