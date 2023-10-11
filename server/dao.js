0129a6469b2c8cfe94da1719ea1c2c4077972f0f
Elijah R <Elijah@R.com>
2023-10-11 11:25:42 -0400

    added basic dao functionality

--- server/dao.js
+++ server/dao.js
@@ -3,9 +3,31 @@ const { MongoClient } = require('mongodb');
 const dbUrl = "mongodb://127.0.0.1:27017";
 const dbName = "twitter2";
 
+/*
 const sample = () => {
     return history;
 }
+*/
 
+async function dbConnect(){
+    let client = new MongoClient(dbUrl);
+    await client.connect();
+    let db = client.db(dbName);
+    return db;
+}
 
-module.exports = { sample }
+//fetch all users
+const getAllUsers = async function(callback){
+    let db = await dbConnect();
+    let dataPromise = db.collection("users").find({}).toArray();
+    dataPromise.then((users) => callback(users));
+}
+
+//fetch user by id
+const getUser = async function(id, callback){
+    let db = await dbConnect();
+    let dataPromise = db.collection("users").findOne({'user_id': id});
+    dataPromise.then((users) => callback(users));
+}
+
+module.exports = { getAllUsers, getUser }
